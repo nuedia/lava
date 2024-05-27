d943f1e1d08c3c9580e5044b38b2b8e011073eeb
nue dia <164995100+nuedia@users.noreply.github.com>
2024-05-28 06:05:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-a66dc352-e774-4861-b3d3-80de0dea1681/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-ffee062a-f8e6-4e0e-bdf9-3d5c77e4692b/)
 
 
 echo "$curl_output" >> Update.tsx
